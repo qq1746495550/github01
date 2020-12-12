d93b74679e3924b3b6a1af50e44b8448715596e6
pengbo <1746495550@qq.com>
2020-12-12 15:26:13 +0800

    1

---
+++ src/main/java/com/p/Github01Application.java
@@ -0,0 +1,13 @@
+package com.p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/p/Github01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Github01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
